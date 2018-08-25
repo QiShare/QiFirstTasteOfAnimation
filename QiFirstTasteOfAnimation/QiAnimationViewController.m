057e2469ea7060108a88ab264c1e18b4703556e8
twototwoto <2637405168@qq.com>
2018-08-25 12:52:11 +0800

    删除上次提交的测试代码

--- QiFirstTasteOfAnimation/QiAnimationViewController.m
+++ QiFirstTasteOfAnimation/QiAnimationViewController.m
@@ -32,7 +32,7 @@
     _basicAniLabel.textColor = [UIColor yellowColor];
     _basicAniLabel.layer.borderWidth = 40.0;
     _basicAniLabel.layer.borderColor = [UIColor blueColor].CGColor;
-    _basicAniLabel.backgroundColor = [UIColor redColor];
+    _basicAniLabel.layer.backgroundColor = [UIColor redColor].CGColor;
     
     _basicAniLabel.layer.shadowOffset = CGSizeZero;
     _basicAniLabel.layer.shadowRadius = 30.0;
@@ -41,26 +41,11 @@
 }
 
 - (void)basicAnimation {
-    
-    _basicAniLabel = [[UILabel alloc] initWithFrame:CGRectMake(.0, .0, 320.0, 320.0)];
-    [self.view addSubview:_basicAniLabel];
-    _basicAniLabel.center = self.view.center;
-    _basicAniLabel.text = @"Q·i Share";
-    _basicAniLabel.textAlignment = NSTextAlignmentCenter;
-    _basicAniLabel.font = [UIFont boldSystemFontOfSize:36.0];
-    _basicAniLabel.textColor = [UIColor yellowColor];
-    _basicAniLabel.layer.borderWidth = 40.0;
-    _basicAniLabel.layer.borderColor = [UIColor blueColor].CGColor;
-    _basicAniLabel.backgroundColor = [UIColor redColor];
-    
-    _basicAniLabel.layer.shadowOffset = CGSizeZero;
-    _basicAniLabel.layer.shadowRadius = 30.0;
-    _basicAniLabel.layer.shadowColor = [UIColor yellowColor].CGColor;
-    _basicAniLabel.layer.shadowOpacity = 1.0;
 
     CABasicAnimation *backColorBasAni = [CABasicAnimation animationWithKeyPath:@"backgroundColor"];
     backColorBasAni.fromValue = (__bridge id _Nullable)([UIColor redColor].CGColor);
-    backColorBasAni.toValue = (__bridge id _Nullable)([UIColor yellowColor].CGColor);
+    backColorBasAni.toValue = (__bridge id _Nullable)([UIColor purpleColor].CGColor);
+    [_basicAniLabel.layer addAnimation:backColorBasAni forKey:@"backgroundColor"];
     
     CABasicAnimation *borderColorBasAni = [CABasicAnimation animationWithKeyPath:@"borderColor"];
     borderColorBasAni.fromValue = (__bridge id _Nullable)([UIColor blueColor].CGColor);
@@ -94,7 +79,6 @@
     CABasicAnimation *shadowOpacityBasAni = [CABasicAnimation animationWithKeyPath:@"shadowOpacity"];
     shadowOpacityBasAni.fromValue = @(1.0);
     shadowOpacityBasAni.toValue = @(0.6);
-    [_basicAniLabel.layer addAnimation:shadowOpacityBasAni forKey:@"shadowOpacity"];
     
     CABasicAnimation *rotateXBasAni = [CABasicAnimation animationWithKeyPath:@"transform.rotation.x"];
     rotateXBasAni.duration = 3.0;
@@ -106,13 +90,13 @@
     rotateYBasAni.duration = 3.0;
     rotateYBasAni.fromValue = @(0);
     rotateYBasAni.toValue = @(M_PI * 2.0);
-    [_basicAniLabel.layer addAnimation:rotateYBasAni forKey:@"transform.rotation.y"];
+     [_basicAniLabel.layer addAnimation:rotateYBasAni forKey:@"transform.rotation.y"];
     
     CABasicAnimation *rotateZBasAni = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
     rotateZBasAni.duration = 3.0;
     rotateZBasAni.fromValue = @(0);
     rotateZBasAni.toValue = @(M_PI * 2);
-    [_basicAniLabel.layer addAnimation:rotateZBasAni forKey:@"transform.rotation.z"];
+     [_basicAniLabel.layer addAnimation:rotateZBasAni forKey:@"transform.rotation.z"];
     
     // 使用组动画的形式可按如下来写
     CAAnimationGroup *animaGroup = [CAAnimationGroup animation];
@@ -128,23 +112,7 @@
 #pragma mark - Private functions
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    
-    // CABasicAnimation *positionAnima = [CABasicAnimation animationWithKeyPath:@"position"];
-    // positionAnima.fromValue = [NSValue valueWithCGPoint:CGPointMake(.0, .0)];
-    // positionAnima.toValue = [NSValue valueWithCGPoint:CGPointMake(100.0, 100.0)];
-    // positionAnima.byValue = [NSValue valueWithCGPoint:CGPointMake(300.0, 300.0)];
-    
-    CABasicAnimation *positionAnima = [CABasicAnimation animationWithKeyPath:@"position"];
-    positionAnima.fromValue = [NSValue valueWithCGPoint:CGPointMake(0.0, 100.0)];
-    positionAnima.byValue = [NSValue valueWithCGPoint:CGPointMake(100.0, 100.0)];
-    positionAnima.toValue = [NSValue valueWithCGPoint:CGPointMake(100.0, 200.0)];
-    positionAnima.removedOnCompletion = NO;
-    positionAnima.fillMode = kCAFillModeForwards;
-    
 
-    positionAnima.duration = 3.0;
-    [_basicAniLabel.layer addAnimation:positionAnima forKey:@"position"];
-    return;
     self.view.userInteractionEnabled = NO;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         self.view.userInteractionEnabled = YES;
@@ -159,6 +127,7 @@
      basAni.duration = 3.0;
      basAni.removedOnCompletion = NO;
      basAni.fillMode = kCAFillModeForwards;
+     backgroundColor、borderColor、borderWidth、cornerRadius、transform.scale、shadowRadius、shadowColor、shadowOpacity、transform.rotation.x、transform.rotation.y、transform.rotation.z
      *注意点:
      * 使用 animationWithKeyPath: 对应的参数一定要写对(包括大小写) 否则就无法出现预期效果
      * 注意设置shadowOffset的值 可以设置为CGSizeZero
@@ -166,8 +135,6 @@
      * 可以使用组动画的形式来做这件事(如果可以达到预期，个人建议这种方式)
      * 也可以一个个的添加动画
      */
-    
-    //backgroundColor、borderColor、borderWidth、cornerRadius、transform.scale、shadowRadius、shadowColor、shadowOpacity、transform.rotation.x、transform.rotation.y、transform.rotation.z都添加了动画
 }
 
 @end
