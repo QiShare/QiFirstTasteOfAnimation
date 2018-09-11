9594e2fd97e35e97315eccbb6a5db32021334902
twototwoto <2637405168@qq.com>
2018-09-11 22:33:51 +0800

    添加autoreleasepool的测试

--- QiFirstTasteOfAnimation/AppDelegate.m
+++ QiFirstTasteOfAnimation/AppDelegate.m
@@ -18,7 +18,39 @@
     _window.backgroundColor = [UIColor whiteColor];
     [_window makeKeyAndVisible];
     
+    [self manualAddAutoreleasePooTest];
+    
     return YES;
 }
+
+
+/**
+ @brief 手动添加autoreleasepool测试
+ */
+- (void)manualAddAutoreleasePooTest {
+    
+    __weak NSObject *weakObj;
+    NSObject *obj1 = [NSObject new];
+    weakObj = obj1;
+    NSLog(@"%@--%@", obj1, weakObj);
+    obj1 = nil;
+    NSLog(@"%@--%@", obj1, weakObj);
+    
+    @autoreleasepool{
+        NSObject *obj2 = [NSObject new];
+        weakObj = obj2;
+        NSLog(@"%@--%@", obj2, weakObj);
+    }
+    NSLog(@"%@", weakObj);
+}
+
+/**
+ Xcode9.4.1 在iPhoneX模拟器及真机12.0(16A5366a)都是这个结果
+ manualAddAutoreleasePooTest输出结果：
+ 2018-09-11 22:20:07.323334+0800 QiFirstTasteOfAnimation[2285:483390] <NSObject: 0x28388b130>--<NSObject: 0x28388b130>
+ 2018-09-11 22:20:07.323421+0800 QiFirstTasteOfAnimation[2285:483390] (null)--(null)
+ 2018-09-11 22:20:07.323471+0800 QiFirstTasteOfAnimation[2285:483390] <NSObject: 0x28388b130>--<NSObject: 0x28388b130>
+ 2018-09-11 22:20:07.323488+0800 QiFirstTasteOfAnimation[2285:483390] (null)
+ */
 
 @end
